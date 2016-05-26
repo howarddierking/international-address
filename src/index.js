4289316ab95036fc35985f27f47646ea2fc5baad
Howard Dierking <howard.dierking@concur.com>
2016-05-26 10:40:33 -0700

    updated case insensitive property selection code to make the comparer function agnostic of what it's being applied to

--- src/index.js
+++ src/index.js
@@ -4,9 +4,16 @@ let cf = require('./countryFormatters');
 let locales = require('./locales');
 let R = require('Ramda');
 
-let compareKeyInsensitive = R.curry(function(comp, val, key){
-  return key.toUpperCase() === comp.toUpperCase();
+let caseInsensitiveComparer = R.curry(function(x, y){
+  return x.toUpperCase() === y.toUpperCase();
 });
+
+// (k -> Boolean) -> {k: v} -> v | undefined
+let propBy = function(pred, object){
+  let w = R.wrap(pred, (fn, val, key) => pred(key));
+  let t = R.pickBy(w, object);
+  return R.nth(0, R.values(t));
+};
 
 exports.formatter = function(defaultFormatter, locale){
   if(arguments.length === 1){
@@ -14,8 +21,6 @@ exports.formatter = function(defaultFormatter, locale){
     defaultFormatter = cf.formatters.unitedStates;
   }
 
-  let localeComparer = compareKeyInsensitive(locale);
-  let foundFormatter = R.pickBy(localeComparer, locales.formatters);
-
-  return R.defaultTo(defaultFormatter, R.nth(0, R.values(foundFormatter)));
+  let v = propBy(caseInsensitiveComparer(locale), locales.formatters); 
+  return R.defaultTo(defaultFormatter, v);
 };
